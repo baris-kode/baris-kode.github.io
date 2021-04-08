d667ba8916e9c7b80c457fe9794572f7aae2d93f
Abdul Rahmad Pasaribu <abdoelrachmad@gmail.com>
2021-04-08 17:06:07 +0700

    feat(top navbar): update top navbar links

--- components/TopNavbar.vue
+++ components/TopNavbar.vue
@@ -6,8 +6,13 @@
       </b-navbar-item>
     </template>
     <template #start>
-      <b-navbar-item :tag="'router-link'" :to="'/'"> Home </b-navbar-item>
-      <b-navbar-item :tag="'router-link'" :to="'/blog'"> Blog </b-navbar-item>
+      <b-navbar-item :tag="'router-link'" :to="'/'"> Beranda </b-navbar-item>
+      <b-navbar-item :tag="'router-link'" :to="'/tutorials'">
+        Tutorial
+      </b-navbar-item>
+      <b-navbar-item :tag="'router-link'" :to="'/docs'">
+        Dokumentasi
+      </b-navbar-item>
     </template>
     <template #end>
       <b-navbar-item :tag="'div'">
